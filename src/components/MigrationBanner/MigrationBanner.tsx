5070689b19d1efbd548288271bc9cd4fff22ee0a
victormer <victormerinokosina@protonmail.com>
2025-02-11 14:53:43 +0100

    migration banner fixes

--- src/components/MigrationBanner/MigrationBanner.tsx
+++ src/components/MigrationBanner/MigrationBanner.tsx
@@ -32,7 +32,7 @@ export function MigrationBanner() {
               <XMarkIcon className={styles.closeIcon} onClick={onClose} />
             </div>
             <LiteYouTubeEmbed
-              id="HaWTWjGPTvI"
+              id="gH6OMrwPqO4"
               title="Migration Video"
               noCookie
               params="autoplay=1"
@@ -40,11 +40,49 @@ export function MigrationBanner() {
           </div>
         </div>
       )}
-      <div className={styles.migrationBannerText}>WE ARE MIGRATING TO $BUU</div>
-      <div className={styles.migrationBannerCA}>
-        <div className={styles.migrationBannerCAText}>{'CA: <CA_ADDRESS>'}</div>
-        <div className={styles.videoIcon}>
-          <VideoCameraIcon onClick={() => setShowVideo(true)} />
+      <div className={styles.left}>
+        <div className={styles.solanaLogo}>
+          <svg
+            className={styles.solanaLogo}
+            width="398"
+            height="312"
+            viewBox="0 0 398 312"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <g clipPath="url(#clip0_347_50)">
+              <path
+                d="M64.6 237.9C67 235.5 70.3 234.1 73.8 234.1H391.2C397 234.1 399.9 241.1 395.8 245.2L333.1 307.9C330.7 310.3 327.4 311.7 323.9 311.7H6.50002C0.700016 311.7 -2.19998 304.7 1.90002 300.6L64.6 237.9Z"
+                fill="white"
+              />
+              <path
+                d="M64.6 3.8C67.1 1.4 70.4 0 73.8 0H391.2C397 0 399.9 7 395.8 11.1L333.1 73.8C330.7 76.2 327.4 77.6 323.9 77.6H6.50002C0.700016 77.6 -2.19998 70.6 1.90002 66.5L64.6 3.8Z"
+                fill="white"
+              />
+              <path
+                d="M333.1 120.1C330.7 117.7 327.4 116.3 323.9 116.3H6.50002C0.700016 116.3 -2.19998 123.3 1.90002 127.4L64.6 190.1C67 192.5 70.3 193.9 73.8 193.9H391.2C397 193.9 399.9 186.9 395.8 182.8L333.1 120.1Z"
+                fill="white"
+              />
+            </g>
+            <defs>
+              <clipPath id="clip0_347_50">
+                <rect width="397.7" height="311.7" fill="white" />
+              </clipPath>
+            </defs>
+          </svg>
+        </div>
+      </div>
+      <div className={styles.right}>
+        <div className={styles.migrationBannerText}>
+          WE ARE MIGRATING TO $BUU
+        </div>
+        <div className={styles.migrationBannerVideoContainer}>
+          <div className={styles.migrationBannerCAText} onClick={() => {}}>
+            {'CA: COMMING SOON'}
+          </div>
+          <div className={styles.videoIcon} onClick={() => setShowVideo(true)}>
+            <VideoCameraIcon />
+          </div>
         </div>
       </div>
     </div>
